0f08173d0e631b8602150cdf0f77f1a3b77d4b88
Quincy Nickens <quincy.j.nickens@gmail.com>
2025-04-18 22:48:29 -0600

    Added basic interface

--- src/main/java/com/legacy/Main.java
+++ src/main/java/com/legacy/Main.java
@@ -1,2 +1,25 @@
 package com.legacy;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) {
+        try {
+            Parent root = FXMLLoader.load(getClass().getResource("/ui/main.fxml"));
+            primaryStage.setTitle("D&D Legacy");
+            primaryStage.setScene(new Scene(root, 800, 600));
+            primaryStage.show();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/main/java/com/legacy/controller/MainController.java
@@ -0,0 +1,4 @@
+package com.legacy.controller;
+
+public class MainController {
+}
